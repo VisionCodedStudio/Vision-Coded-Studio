a08bb3aa78f8adf07a47763d3bfac158ce8d4d8d
Vison Coded Studio <visioncodedstudio@gmail.com>
2026-03-11 09:08:25 -0700

    Update cart.js

--- fitness-website/cart.js
+++ fitness-website/cart.js
@@ -1,1 +1,29 @@
+let cart = []
 
+const cartCount = document.getElementById("cart-count")
+
+document.querySelectorAll(".add-cart").forEach(button => {
+
+button.addEventListener("click", () => {
+
+const productCard = button.closest(".product-card")
+
+const productName = productCard.querySelector("h3").innerText
+const productPrice = productCard.querySelector(".price").innerText
+
+const item = {
+name: productName,
+price: productPrice
+}
+
+cart.push(item)
+
+cartCount.innerText = cart.length
+
+console.log(cart)
+
+alert(productName + " added to cart")
+
+})
+
+})
